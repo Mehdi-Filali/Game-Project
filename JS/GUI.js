754ca3156e1b4c323d0c5309423260c2dc3a1e03
mehdi Aniq-Filali <mehdi Aniq-Filali>
2016-12-05 03:19:27 +0100

    set timer

--- JS/GUI.js
+++ JS/GUI.js
@@ -1,19 +1,21 @@
 /* Home */
-var body = document.getElementsByTagName('body')[0];
+
 var play=document.getElementById('button-play');
 
 var playing = function(){
     document.getElementById('button-play').style.display= 'none';
+    
     document.getElementById('button-exit').style.display= 'none';
-    document.getElementById('button-player').style.display= 'inline-block';
+        
+    document.getElementById('timer').style.display= 'inline-block';
 }
 
 play.addEventListener('click', playing);
 
-var player=document.getElementById('button-player');
+var player=document.getElementById('timer');
 
 var lets_play = function(){
-    document.getElementById('head').style.display='none';
+    document.getElementById('head').style.display='inline-block';
     document.getElementById('window-game').style.display='inline-block';
 }
 
@@ -33,58 +35,36 @@ exit.addEventListener('click', quit);
 var randomize = function() {
     var blocs = document.getElementsByClassName("bloc");
     for(var b = 0; b < blocs.length; b++){
-        blocs[b].style.height = Math.random() * (55 - 30) + 30;
-        blocs[b].style.width = Math.random() * (45 - 10) + 10;
+        blocs[b].style.height = Math.random() * (50 - 30) + 30;
+        blocs[b].style.width = Math.random() * (40 - 20) + 20;
      }
 }
 
 setInterval(randomize,1000);
 
-var jump = document.getElementById('Stickman1');
 
-function key_down(e){
-    console.log(e.keyCode);
-    if(e.keyCode === 38){
-        jump.style.top = '-70px'; 
+/* Timer */
+
+var interval,
+    c = 3,
+    s = document.getElementById('timer');
+
+function countdown(){
+    s.innerHTML = c;
+    
+    if(c > 0){
+        c--;
+        console.log(c);
+    }else{
+        location=('Index2.0.html');
+        clearInterval(interval);
+        s.innerHTML = 'GO!';
     }
 }
 
-body.addEventListener('keydown', key_down); 
-
-function key_up(e){
-    if(e.keyCode === 38){
-        jump.style.top = '0px';
-    }
+function ilan(){
+    s.innerHTML = c;
+    interval = setInterval(countdown, 1000);
 }
-
-body.addEventListener('keyup', key_up);
-
-
-/* Game Over 
-var position = function(){
-    var stick = document.getElementById('Stickman1');
-    var blocs_collision=document.getElementsByClassName('bloc');
-    if((stick.offsetLeft - blocs_collision.offsetLeft < 50) && (stick.offsetLeft - blocs_collision.offsetLeft > -50)){
-       console.log("alignés !");
-       
-   }
-}
-position();
-*/
-
-/* Score */
-
-var t = document.getElementById('score');
-
-var score_evolution = function(){
-    t.innerHTML = c;
-}
-
-
-
-
-
-
-
-
+s.addEventListener('click', ilan);
 
