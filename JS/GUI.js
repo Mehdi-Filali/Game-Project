445f0cccf823e0ddc24a7b9d195dad5926cb4726
IlanZerdoun <ilanzerdoun@ic.home>
2016-12-11 14:30:32 +0100

    New update

--- JS/GUI.js
+++ JS/GUI.js
@@ -58,7 +58,7 @@ function key_down(e){
             return;
         }
         
-        jump.style.top = '85px'; 
+        jump.style.top = '80px'; 
         flag=true;
     }
 }
@@ -99,4 +99,55 @@ function ilan(){
 }
 
 if(s) s.addEventListener('click', ilan);
+
+/* Collision */
+
+var check_collision = function(){
+    var _s = document.getElementById('Stickman1'),
+        _blocs = document.getElementsByClassName('bloc'),
+        _end = document.getElementById('game-over'),
+        _restart = document.getElementById('restart'),
+        _return = document.getElementById('exit');
+    
+    for (var i= 0; i<_blocs.length ; i++){
+        var _bloc = _blocs[i];
+    
+        if(_bloc.offsetLeft <= (_s.offsetLeft + _s.offsetWidth)){
+            if((_s.offsetTop + _s.offsetHeight) > _bloc.offsetTop ){ 
+                _s.style.display = 'none' ;
+                for (var j=0; j<_blocs.length; j++){
+                    _blocs[j].style.display = 'none' ;
+                }
+                _end.style.display = 'inline-block';
+                _restart.style.display = 'inline-block';
+                _return.style.display = 'inline-block';
+                clearInterval(interval);;
+            }
+        }
+        
+    }
+}
+
+setInterval(check_collision, 200);
+
+
+/* Score */
+
+var interval,       
+    score = document.getElementById('score'),
+    evolution = 0;
+
+var evolution_score = function(){
+    do{
+        evolution++;
+        score.innerHTML = evolution;
+    }while(s);
+}
+
+
+var decompte = function(){
+    interval = setInterval(evolution_score, 900);
+}
+
+setTimeout(decompte, 3200);
 
